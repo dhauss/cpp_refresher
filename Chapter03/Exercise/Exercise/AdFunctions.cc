65e10076c568de270a878f34c5186b1ad4281e35
Dave <davidkhauss@gmail.com>
2024-06-20 14:53:52 +0000

    quick start to modern C++ base code

---
+++ Chapter03/Exercise/Exercise/AdFunctions.cc
@@ -0,0 +1,30 @@
+#include <cmath>
+#include <cstdint>
+#include <iostream>
+
+#include "AdConstants.h"
+#include "AdFunctions.h"
+
+namespace Ad
+{
+namespace Visualize
+{
+
+void print_vehicle(const Ad::Types::VehicleType &vehicle)
+{
+
+}
+
+void print_neighbor_vehicles(const Ad::Types::NeighborVehiclesType &vehicles)
+{
+
+}
+
+void print_scene(const Ad::Types::VehicleType &ego_vehicle,
+                 const Ad::Types::NeighborVehiclesType &vehicles)
+{
+
+}
+
+} // namespace Visualize
+} // namespace Add
---
+++ Chapter06/Exercise/Exercise/algo.cpp
@@ -0,0 +1,30 @@
+#include "algo.hpp"
+
+namespace mystd
+{
+
+bool equal(Iterator first1, Iterator last1, Iterator first2)
+{
+    return true;
+}
+
+Iterator fill_n(Iterator first, std::size_t count, const std::int32_t &value)
+{
+    return first;
+}
+
+void iota(Iterator first, Iterator last, std::int32_t value)
+{
+}
+
+Iterator copy(Iterator first, Iterator last, Iterator d_first)
+{
+    return d_first;
+}
+
+std::int32_t accumulate(Iterator first, Iterator last, std::int32_t init)
+{
+    return init;
+}
+
+} // namespace mystd
